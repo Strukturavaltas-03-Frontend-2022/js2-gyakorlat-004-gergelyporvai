7c15f0663996ae163f8460f662f5569059d3b5aa
GergelyPorvai <porvai.gergely@gmail.com>
2022-08-22 01:12:39 +0200

    kész, teszt olyat is kért ami kiírásban nem volt szerintem

--- solutions/getSecondsOfThisYearUntilNow.js
+++ solutions/getSecondsOfThisYearUntilNow.js
@@ -1,3 +1,14 @@
+/* const getSecondsOfThisYearUntilNow = () => {
+  const today = new Date();
+  const yearNow = today.getFullYear();
+  const thisYearNewYear = new Date(yearNow, 0, 1);
+  return (Date.now() - thisYearNewYear.getTime()) / 1000;
+};*/
+
+const getSecondsOfThisYearUntilNow = () => {
+  const today = new Date();
+  const thisYearNewYear = new Date(today.getFullYear(), 0, 1);
+  return (Date.now() - thisYearNewYear.getTime()) / 1000;
+};
 
 export default getSecondsOfThisYearUntilNow;
-
